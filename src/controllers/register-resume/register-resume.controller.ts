175e352f79ec81ba1605fc24ac7440d8b2fdb88f
flama <flamarionpinto@sou.faccat.br>
2023-11-28 12:02:40 -0300

    feat: validat full name

--- src/controllers/register-resume/register-resume.controller.ts
+++ src/controllers/register-resume/register-resume.controller.ts
@@ -3,8 +3,22 @@ import { RegisterResumeService } from "@/services";
 import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 
+const fullNameValidator = (value: string): boolean => {
+  try {
+    const names = value.split(" ");
+    const isValidLastName = names[1]?.trim().length > 0;
+
+    return names.length >= 2 && isValidLastName;
+  } catch (error) {
+    return false;
+  }
+};
+
 const bodySchema = z.object({
-  full_name: z.string().regex(/^[a-zA-Z\s]+$/),
+  full_name: z
+    .string()
+    .regex(/^[a-zA-Z\s]+$/)
+    .refine(fullNameValidator, "É necessário informar o nome completo"),
   email: z.string().email(),
   phone_number: z.string().min(10).max(11).regex(/^\d+$/).optional(),
   web_address: z.string().url().optional(),
